343f8d4f42e4e7a4851899f11d9eb2954471a17f
Lukasz Dalek <dalek.lukasz@gmail.com>
2022-12-16 22:00:32 +0100

    sphinx: Remove apt cache

--- sphinx/cmd.sh
+++ sphinx/cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 export DEBIAN_FRONTEND="noninteractive"
 export TZ="Etc/UTC"
 
@@ -7,3 +9,6 @@ apt-get -y update
 apt-get -y install \
 	python-is-python3 \
 	python3-sphinx python3-sphinx-rtd-theme
+
+# Remove unneeded stuff
+rm -rf /var/lib/apt /var/lib/dpkg
